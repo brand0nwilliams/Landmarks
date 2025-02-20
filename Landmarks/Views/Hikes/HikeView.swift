7238f383af695ce95741ba4935d8f36beff3f164
brand0nwilliams <bpwilliams13@gmail.com>
2025-02-20 11:21:34 -0500

    Animating views and transitions

--- Landmarks/Views/Hikes/HikeView.swift
+++ Landmarks/Views/Hikes/HikeView.swift
@@ -7,9 +7,18 @@ A view displaying information about a hike, including an elevation graph.
 
 import SwiftUI
 
+extension AnyTransition {
+    static var moveAndFade: AnyTransition {
+        .asymmetric(
+            insertion: .move(edge: .trailing).combined(with: .opacity),
+            removal: .scale.combined(with: .opacity)
+        )
+    }
+}
+
 struct HikeView: View {
     var hike: Hike
-    @State private var showDetail = false
+    @State private var showDetail = true
 
     var body: some View {
         VStack {
@@ -26,26 +35,29 @@ struct HikeView: View {
                 Spacer()
 
                 Button {
-                    showDetail.toggle()
+                    withAnimation {
+                        showDetail.toggle()
+                    }
                 } label: {
                     Label("Graph", systemImage: "chevron.right.circle")
                         .labelStyle(.iconOnly)
                         .imageScale(.large)
                         .rotationEffect(.degrees(showDetail ? 90 : 0))
-                        .hueRotation(.degrees(showDetail ? 45 : 0))
-                        .shadow(color: showDetail ? .blue.opacity(0.5) : .clear, radius: 5)
-                        .animation(.easeInOut(duration: 1.3), value: showDetail)
+//                        .hueRotation(.degrees(showDetail ? 45 : 0))
+//                        .shadow(color: showDetail ? .blue.opacity(0.5) : .clear, radius: 5)
+//                        .animation(.easeInOut(duration: 1.3), value: showDetail)
                         .scaleEffect(showDetail ? 1.5 : 1)
-                        .offset(y: showDetail ? -5 : 0)
-                        .animation(.spring(response: 1.5, dampingFraction: 0.5), value: showDetail)
+//                        .offset(y: showDetail ? -5 : 0)
+//                        .animation(.spring(response: 1.5, dampingFraction: 0.5), value: showDetail)
                         .padding()
-                        .animation(.spring(), value: showDetail)
+//                        .animation(.spring(), value: showDetail)
 
                 }
             }
 
             if showDetail {
                 HikeDetail(hike: hike)
+                    .transition(.moveAndFade)
             }
         }
     }
